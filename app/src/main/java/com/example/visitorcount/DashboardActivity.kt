449c375b101126084f854116c76fac7f72d3e018
jhemapca <jhemapca@gmail.com>
2023-06-13 21:28:44 -0500

    No funciona

--- app/src/main/java/com/example/visitorcount/DashboardActivity.kt
+++ app/src/main/java/com/example/visitorcount/DashboardActivity.kt
@@ -10,17 +10,25 @@ import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.anychart.charts.Pie
+import com.anychart.core.cartesian.series.Column
 import com.anychart.enums.Align
+import com.anychart.enums.Anchor
+import com.anychart.enums.HoverMode
 import com.anychart.enums.LegendLayout
+import com.anychart.enums.Position
+import com.anychart.enums.TooltipPositionMode
+import com.anychart.graphics.vector.Stroke
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import java.io.File
 import java.io.FileWriter
 
 class DashboardActivity : AppCompatActivity() {
-    private val STORAGE_PERMISSION_REQUEST_CODE = 1
+    private val STORAGE_PERMISSION_REQUEST_CODE = 123
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -28,7 +36,10 @@ class DashboardActivity : AppCompatActivity() {
         val db= FirebaseFirestore.getInstance()
         val grafico:AnyChartView = findViewById(R.id.any_chart_view)
         val pie:Pie=AnyChart.pie()
+        val grafico2:AnyChartView=findViewById(R.id.bar_chart_view)
+        val cartesian=AnyChart.column()
         btnDownload.setOnClickListener{
+
             db.collection("Entrada").addSnapshotListener{snapshots,e->
 
                 if (e!=null){
@@ -56,92 +67,11 @@ class DashboardActivity : AppCompatActivity() {
                     "Archivo Excel descargado",
                     Toast.LENGTH_LONG).show()
             }
-
-//            db.collection("Entrada").get()
-//                .addOnSuccessListener { entradaDocuments ->
-//                    // Obtener los datos de la colección "Salida"
-//                    db.collection("Salida").get()
-//                        .addOnSuccessListener { salidaDocuments ->
-//                            // Crear un nuevo libro de trabajo de Excel
-//                            val csvFile = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "datos.csv")
-//                            val csvWriter = FileWriter(csvFile)
-//
-//                            csvWriter.append("anio")
-//                            csvWriter.append(",")
-//                            csvWriter.append("mes")
-//                            csvWriter.append(",")
-//                            csvWriter.append("dia")
-//                            csvWriter.append(",")
-//                            csvWriter.append("hora")
-//                            csvWriter.append(",")
-//                            csvWriter.append("minuto")
-//                            csvWriter.append(",")
-//                            csvWriter.append("segundo")
-//                            csvWriter.append(",")
-//                            csvWriter.append("sexo")
-//                            csvWriter.append("\n")
-//
-//                            // Recorrer los documentos de la colección "Entrada" y agregar los datos a Excel
-//                            for (entradaDocument in entradaDocuments) {
-//                                val rowData = entradaDocument.data
-//                                csvWriter.append(rowData["anio"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["mes"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["dia"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["hora"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["minuto"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["segundo"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["sexo"].toString())
-//                                csvWriter.append("\n")
-//                            }
-//                            for (salidaDocument  in salidaDocuments) {
-//                                val rowData = salidaDocument.data
-//                                csvWriter.append(rowData["anio"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["mes"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["dia"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["hora"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["minuto"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["segundo"].toString())
-//                                csvWriter.append(",")
-//                                csvWriter.append(rowData["sexo"].toString())
-//                                csvWriter.append("\n")
-//                            }
-//                            csvWriter.flush()
-//                            csvWriter.close()
-//
-//                            Toast.makeText(
-//                                this,
-//                                "Archivo Excel descargado",
-//                                Toast.LENGTH_LONG
-//                            ).show()
-//                        }
-//                        .addOnFailureListener { exception ->
-//                            Toast.makeText(
-//                                this,
-//                                "Error al obtener los datos de la colección 'Salida'",
-//                                Toast.LENGTH_LONG
-//                            ).show()
-//                        }
-//                }
-//                .addOnFailureListener { exception ->
-//                    Toast.makeText(
-//                        this,
-//                        "Error al obtener los datos de la colección 'Entrada'",
-//                        Toast.LENGTH_LONG
-//                    ).show()
-//                }
         }
-        val data:ArrayList<DataEntry> = ArrayList()
+        val dataPie:ArrayList<DataEntry> = ArrayList()
+        val dataBarHE:ArrayList<DataEntry> = ArrayList()
+        val horas:Array<String> = arrayOf("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23")
+        var contHE:MutableList<Int> = mutableListOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 
         db.collection("Entrada").addSnapshotListener{snapshots,e->
             if (e!=null){
@@ -154,17 +84,26 @@ class DashboardActivity : AppCompatActivity() {
                     DocumentChange.Type.ADDED,
                     DocumentChange.Type.MODIFIED,
                     DocumentChange.Type.REMOVED->{
+
                         if(dc.document.data["sexo"].toString()=="Hombre"){
                             h += 1
                         }else{
                             m += 1
                         }
+                        for (i in 0..23){
+                            if(dc.document.data["hora"].toString()==horas[i]){
+                                contHE[i]+=1
+                            }
+                        }
                     }
                 }
             }
-            data.add(ValueDataEntry("Hombres",h))
-            data.add(ValueDataEntry("Mujeres",m))
-            pie.data(data)
+            for(i in 0..23){
+                dataBarHE.add(ValueDataEntry(horas[i],contHE[i]))
+            }
+            dataPie.add(ValueDataEntry("Hombres",h))
+            dataPie.add(ValueDataEntry("Mujeres",m))
+            pie.data(dataPie)
         }
 
 
@@ -181,6 +120,22 @@ class DashboardActivity : AppCompatActivity() {
             .align(Align.CENTER);
 
         grafico.setChart(pie)
+
+        var column: Column = cartesian.column(dataBarHE)
+        column.tooltip()
+            .titleFormat("{%X}")
+            .position(Position.CENTER_BOTTOM)
+            .anchor(Anchor.CENTER_BOTTOM)
+            .offsetX(0.0)
+            .offsetY(5.0)
+            .format("${"%"}{groupsSeparator: }")
+        cartesian.animation(true)
+        cartesian.title("Entrada de hombres por hora")
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT)
+        cartesian.interactivity().hoverMode(HoverMode.BY_X)
+        cartesian.xAxis(0).title("Horas")
+        cartesian.yAxis(0).title("N°Personas")
+        grafico2.setChart(cartesian)
 
     }
 }
